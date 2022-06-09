aba2e806c7cf66bbc920dbf0c26a58241bab313a
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-09 16:11:40 +0530

    JDBC

--- src/main/java/com/capgemini/demo/jdbc/JdbcDemo.java
+++ src/main/java/com/capgemini/demo/jdbc/JdbcDemo.java
@@ -4,6 +4,7 @@ import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 // jdbc demo to connect to Postgres DB 
 // if your PG admin is open, 
@@ -17,7 +18,7 @@ public class JdbcDemo {
 		String url = "jdbc:postgresql://localhost:5432/may12";
 		String user = "postgres";
 		String password = "root";
-		String sql = "SELECT * FROM emps";
+		String sql = "SELECT * FROM emps;";
 
 		Connection con = null;
 		Statement st = null;
@@ -26,16 +27,15 @@ public class JdbcDemo {
 		System.out.println("Start");
 		try {
 
-			Class.forName(className);
+//			Class.forName(className);
 			con = DriverManager.getConnection(url, user, password);
 			st = con.createStatement();
 			rs = st.executeQuery(sql);
 
 			while (rs.next()) {
-				int index = 1;
-				for (int i = 1; i <= rs.getFetchSize(); i++) {
-					System.out.print(rs.getString(index) + " \t");
-					index++;
+
+				for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+					System.out.print(rs.getString(i) + " \t");
 				}
 				System.out.println("\n");
 			}
