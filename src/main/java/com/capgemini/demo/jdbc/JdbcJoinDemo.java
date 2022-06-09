2055363ec93fb970b30134863f1c8e3d028d0402
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-09 16:57:18 +0530

    JDBC

---
+++ src/main/java/com/capgemini/demo/jdbc/JdbcJoinDemo.java
@@ -0,0 +1,51 @@
+package com.capgemini.demo.jdbc;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class JdbcJoinDemo {
+
+	public static void main(String[] args) {
+
+		String className = "org.postgresql.Driver";
+		String url = "jdbc:postgresql://localhost:5432/may12";
+		String user = "postgres";
+		String password = "root";
+		String sql = "SELECT e.eid, e.first_name, e.salary, \r\n" + "d.did, d.dname, d.city \r\n" + "FROM emps e\r\n"
+				+ "FULL JOIN deps d\r\n" + "ON e.did = d.did;\r\n" + "";
+
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		System.out.println("Start");
+		try {
+
+			Class.forName(className);
+			con = DriverManager.getConnection(url, user, password);
+			st = con.createStatement();
+			rs = st.executeQuery(sql);
+
+			for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+				System.out.print(rs.getMetaData().getColumnLabel(i) + " \t");
+			}
+			System.out.println("\n");
+			while (rs.next()) {
+
+				for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+					System.out.print(rs.getString(i) + " \t");
+				}
+				System.out.println("");
+			}
+			rs.close();
+			st.close();
+			con.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("End");
+	}
+}
