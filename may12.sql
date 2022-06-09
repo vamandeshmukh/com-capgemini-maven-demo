addeaf6bc31eccb4f5ba5bb381e86e01c9d24727
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-09 09:46:09 +0530

    SQL

--- may12.sql
+++ may12.sql
@@ -30,10 +30,80 @@ SELECT first_name FROM emp;
 SELECT first_name, salary FROM emp;
 SELECT eid, first_name, salary FROM emp;
 
+-- emps with salary > 60K
+SELECT eid, first_name, salary 
+FROM emp 
+WHERE salary > 60000
+ORDER BY first_name;
+
+-- Constraints in SQL 
+-- PK, FK, Check, NN, Unique 
+
+-- emps 
+
+CREATE TABLE emps(eid INT constraint emps_pk primary key, 
+				  first_name varchar(10), 
+				  salary DECIMAL);
+
+INSERT INTO emps (eid, first_name, salary) VALUES (101, 'Sonu', 50000);
+INSERT INTO emps (eid, first_name, salary) VALUES (102, 'Monu', 60000);
+INSERT INTO emps (eid, first_name, salary) VALUES (103, 'Ponu', 55000);
+
+COMMIT;
+
+SELECT * FROM emps;
+
+-- try to insert duplicate record 
+INSERT INTO emps (eid, first_name, salary) VALUES (102, 'Monu', 60000);
+
+COMMIT;
+
+-- try to insert null pk record 
+INSERT INTO emps (eid, first_name, salary) VALUES (null, 'Monu', 60000);
+
+COMMIT;
 
 
+DROP TABLE emps;
 
 
+CREATE TABLE deps(did INT constraint deps_pk primary key, 
+				  dname varchar(40), 
+				  city varchar(40));
+
+COMMIT; 
+
+CREATE TABLE emps(eid INT constraint emps_pk primary key, 
+				  first_name varchar(10), 
+				  salary DECIMAL, 
+				  did INT CONSTRAINT emps_deps_fk 
+				  references deps(did)
+				 );
+
+COMMIT; 
+
+INSERT INTO deps (did, dname, city) VALUES (10, 'HR', 'Hyderabad');
+INSERT INTO deps (did, dname, city) VALUES (20, 'Admin', 'Bengaluru');
+INSERT INTO deps (did, dname, city) VALUES (30, 'Marketing', 'Chennai');
+INSERT INTO deps (did, dname, city) VALUES (40, 'Development', 'Pune');
+
+
+COMMIT; 
+
+INSERT INTO emps (eid, first_name, salary, did) VALUES (101, 'Sonu', 50000, 10);
+INSERT INTO emps (eid, first_name, salary, did) VALUES (102, 'Monu', 60000, 10);
+INSERT INTO emps (eid, first_name, salary, did) VALUES (103, 'Tonu', 55000, 20);
+INSERT INTO emps (eid, first_name, salary, did) VALUES (104, 'Ponu', 75000, 30);
+INSERT INTO emps (eid, first_name, salary, did) VALUES (105, 'Gonu', 70000, NULL);
+
+COMMIT; 
+
+SELECT * FROM emps;
+SELECT * FROM deps;
+
+-- try to insert a wrong record
+
+INSERT INTO emps (eid, first_name, salary, did) VALUES (106, 'Ronu', 50000, 50);
 
 
  
