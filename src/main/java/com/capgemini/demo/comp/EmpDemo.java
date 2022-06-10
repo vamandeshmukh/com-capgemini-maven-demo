8cb485f4b9d339d60cd8964fa4a7018b54cdd0ab
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-10 12:31:41 +0530

    JDBC

---
+++ src/main/java/com/capgemini/demo/comp/EmpDemo.java
@@ -0,0 +1,54 @@
+package com.capgemini.demo.comp;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EmpDemo {
+
+	public static void main(String[] args) {
+
+		Employee emp = new Employee(102, "Monu", 60000);
+
+		System.out.println(emp.toString());
+
+		Employee emp2 = new Employee(102, "MonuMonuMonu", 60000);
+		System.out.println(emp2.toString());
+
+		List<Employee> empList = new ArrayList<>();
+		empList = new LinkedList<>();
+
+		ArrayList<Employee> empList2 = new ArrayList<>();
+//		empList2 = new LinkedList<>();
+		
+		
+//		JPA obj = new Hibernate();
+//		obj = new SpringData();
+//		Hibernate obj2 = new Hibernate();
+//		obj2 = new SpringData();
+		
+		
+		
+
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
